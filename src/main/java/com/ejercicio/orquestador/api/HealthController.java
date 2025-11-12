7d7dcaf89676c29a739e5ce794b924a92e2e36c1
lanza11 <mateo.vasquezg@udea.edu.co>
2025-11-11 22:55:31 -0500

    agregando prefijo api/

--- src/main/java/com/ejercicio/orquestador/api/HealthController.java
+++ src/main/java/com/ejercicio/orquestador/api/HealthController.java
@@ -1,10 +1,12 @@
 package com.ejercicio.orquestador.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@RequestMapping("api")
 public class HealthController {
     @GetMapping("/ping")
     public Map<String, String> ping() {
